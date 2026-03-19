e026314848a5a426c584b227f4cd5160db32ae20
Marlyson Rodrigues Souza <marlyson.5019@gmail.com>
2026-03-19 12:21:01 -0300

    Add MainFuncionario class with main method

---
+++ exercicio5/MainFuncionario.java
@@ -0,0 +1,9 @@
+package com.mycompany.construtor;
+
+public class MainFuncionario {
+    public static void main(String[] args) {
+        Funcionario f = new Funcionario();
+        f.leDados();
+        f.MsgdeInforma();
+    }
+}
